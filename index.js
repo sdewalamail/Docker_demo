8ca842a06ce6cbec7be2d9dc550881eb2e750fea
sdewalamail <sdewalamail@gmail.com>
2024-03-24 12:12:27 +0530

    added server file for docker demo

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+app.get("/", (req, res) => { console.log("requset is comming on server."); return res.send("<h2>Hello champ!</h2>") })
+
+function startServer() {
+    app.listen( PORT, () => { console.log(`Server is running on port ${PORT}`) });
+};
+
+startServer()
+
+
